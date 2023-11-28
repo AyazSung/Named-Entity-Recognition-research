05e42d6bcf7db5d10b80253bcb661dc3064c27dc
Marat <freiden2004@gmail.com>
2023-11-28 15:23:57 +0300

    Add spacy to approaches from main branch

---
+++ approaches/SPACY/calling_the_model.py
@@ -0,0 +1,8 @@
+from model_class import *
+
+st = SpacyTrained()
+
+doc = st.predict_entities("Microsoft company was founded in 1945 year by Bill Gates and Paul Allen")
+
+for ent in doc.ents:
+    print(ent.text, ent.label_)
